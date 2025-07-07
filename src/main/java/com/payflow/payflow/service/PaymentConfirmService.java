12de3232c9754f1aaf9e5ca4e822a39ccce7591c
alsduq1117 <alsduq1117@naver.com>
2025-07-08 02:47:39 +0900

    chore : PaymentConfirmService 로그 삭제

--- src/main/java/com/payflow/payflow/service/PaymentConfirmService.java
+++ src/main/java/com/payflow/payflow/service/PaymentConfirmService.java
@@ -1,12 +1,9 @@
 package com.payflow.payflow.service;
 
 import com.payflow.payflow.dto.request.TossPaymentConfirmRequest;
-import com.payflow.payflow.dto.response.PaymentConfirmationResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +13,6 @@ public class PaymentConfirmService {
     private final TossPaymentExecutor tossPaymentExecutor;
 
     public String confirm(TossPaymentConfirmRequest request) {
-        log.info("PaymentConfirmService 수행");
         return tossPaymentExecutor.execute(request);
     }
 }
