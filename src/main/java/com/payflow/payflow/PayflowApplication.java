ed25a4f9d44fba3c0f68dfc62df5beb81717cd83
alsduq1117 <alsduq1117@naver.com>
2025-08-21 05:26:01 +0900

    test : ci/cd test

--- src/main/java/com/payflow/payflow/PayflowApplication.java
+++ src/main/java/com/payflow/payflow/PayflowApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PayflowApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PayflowApplication.class, args);
     }
-
 }
