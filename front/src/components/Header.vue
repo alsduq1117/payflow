f830a4fab5837307b0abaf0f0bc31bd57dd55ede
alsduq1117 <alsduq1117@naver.com>
2025-08-21 05:09:29 +0900

    chore : ci/cd test

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import {ref, onMounted, onUnmounted, computed} from 'vue';
-import { useAuthStore } from '@/stores/auth.ts';
-import { storeToRefs } from 'pinia';
+import {useAuthStore} from '@/stores/auth.ts';
+import {storeToRefs} from 'pinia';
 import HeaderNav from './HeaderNav.vue';
 import HeaderAuth from './HeaderAuth.vue';
 import axios from "axios";
 
 const auth = useAuthStore();
-const { accessToken } = storeToRefs(auth);
+const {accessToken} = storeToRefs(auth);
 const isLoggedIn = computed(() => !!accessToken.value);
 const isScrolled = ref(false);
 
@@ -36,8 +36,8 @@ onUnmounted(() => {
 <template>
   <v-app-bar app height="52" color="white" :elevation="isScrolled ? 2 : 0" flat>
     <v-container fluid class="header-container">
-      <HeaderNav />
-      <HeaderAuth :is-logged-in="isLoggedIn" @logout="logout" />
+      <HeaderNav/>
+      <HeaderAuth :is-logged-in="isLoggedIn" @logout="logout"/>
     </v-container>
   </v-app-bar>
 </template>
