9ca9288cf88bc869b82deb9a82dddb654a54dd78
alsduq1117 <alsduq1117@naver.com>
2025-08-21 23:19:14 +0900

    fix : 모니터링 기능 배포시 안되는것 수정 chore : 운영자만 볼 수 있도록 관리자 페이지 설정한것 수정

--- src/main/java/com/payflow/payflow/controller/admin/AdminController.java
+++ src/main/java/com/payflow/payflow/controller/admin/AdminController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
-@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final AdminService adminService;
